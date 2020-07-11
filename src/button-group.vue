da96dde1e5f994cb0c8f4ff27f7facdd3aa6df0c
layouwen <layouwen@gmail.com>
2020-07-11 21:13:34 +0800

    检测 button-group 中的元素是否合法

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
-    export default {}
+    export default {
+        mounted() {
+            for(let node of this.$el.children) {
+                let name = node.nodeName.toLowerCase()
+                if(name != 'button') {
+                    console.warn(`g-button-group 中应该只含有 button, 而你的是 ${name}`)
+                }
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
