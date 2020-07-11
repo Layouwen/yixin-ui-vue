47c9bfd0b71a7a00eedf61f569352fa1c843d1a9
layouwen <layouwen@gmail.com>
2020-07-11 19:38:31 +0800

    将 svg 代码整理到 icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
         <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-            <svg v-if="icon" class="icon">
-                <use :xlink:href="`#icon-${icon}`"></use>
-            </svg>
+            <g-icon v-if="icon" :name="icon"></g-icon>
             <div class="content">
                 <slot></slot>
             </div>
