45e47cef79d9042022b99384cdcb372cd58e9e9b
layouwen <layouwen@gmail.com>
2020-07-11 17:53:58 +0800

    将 button 组件改为vue单文件组件

--- button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('g-button', { // 声明全局组件g-button
-    template: `
-        <button class="g-button">按钮</button>
-    `
-});
---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+    .g-button {
+        padding: 0 1em;
+        height: var(--button-height);
+        font-size: var(--font-size);
+        border: 1px solid var(--border-color);
+        border-radius: var(--border-radius);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--border-color-hover);
+    }
+        &:active {
+            background-color: var(--button-active-bg);
+    }
+        &:focus {
+            outline: none;
+     }
+    }
+</style>
