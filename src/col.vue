c9d2ee11ae3e71f5398550c60ad108d449a442ce
layouwen <layouwen@gmail.com>
2020-07-16 23:09:59 +0800

    创建col 和 row组件

---
+++ src/col.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "YixinCol"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "YixinRow"
+    }
+</script>
+
+<style scoped>
+
+</style>
