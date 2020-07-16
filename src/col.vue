7bba5277dfa6e934b311eafb05c121acfc9535fd
layouwen <layouwen@gmail.com>
2020-07-17 00:48:54 +0800

    大致实现栅格布局

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+    :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+        <div style="border: 1px solid green; height: 100px;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
@@ -10,6 +13,14 @@
         props: {
             span: {
                 type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
+            }
+        },
+        data(){
+            return {
+                gutter: 0
             }
         }
     }
@@ -19,13 +30,17 @@
     .col {
         height: 100px;
         width: 50%;
-        border: 1px solid red;
-        background: blue;
 
         $class-prefix: col-;
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: ($n /24) * 100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n /24) * 100%;
             }
         }
     }
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,23 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "YixinRow"
+        name: "YixinRow",
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        mounted(){
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter
+            })
+            console.log(this.gutter)
+        }
     }
 </script>
 
