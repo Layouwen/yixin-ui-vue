935a7096cea7befb8ffc4b6078fb8d91aac7c870
layouwen <layouwen@gmail.com>
2020-07-17 14:53:42 +0800

    添加 row 的 left center right

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row" :style="rowStyle">
+    <div class="row" :style="rowStyle" :class="rowClass">
         <slot></slot>
     </div>
 </template>
@@ -10,19 +10,29 @@
         props: {
             gutter: {
                 type: [Number, String]
+            },
+            align: {
+                type: String,
+                validator(value) {
+                    return ['left', 'right', 'center'].includes(value)
+                }
             }
         },
         computed: {
-          rowStyle() {
-              let {gutter} = this
-              return {
-                  marginLeft: -gutter/2+'px',
-                  marginRight: -gutter/2+'px'
-              }
-          }
+            rowStyle() {
+                let {gutter} = this
+                return {
+                    marginLeft: -gutter / 2 + 'px',
+                    marginRight: -gutter / 2 + 'px'
+                }
+            },
+            rowClass() {
+                let {align} = this
+                return [align && `align-${align}`]
+            }
         },
-        mounted(){
-            this.$children.forEach((vm)=>{
+        mounted() {
+            this.$children.forEach((vm) => {
                 vm.gutter = this.gutter
             })
         }
@@ -33,5 +43,17 @@
     .row {
         display: flex;
         flex-wrap: wrap;
+
+        &.align-left {
+            justify-content: flex-start;
+        }
+
+        &.align-right {
+            justify-content: flex-end;
+        }
+
+        &.align-center {
+            justify-content: center;
+        }
     }
 </style>
