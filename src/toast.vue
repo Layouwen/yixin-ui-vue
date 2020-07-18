233b96df4353113e7f8cdf8b954a2d0341e9a52f
layouwen <layouwen@gmail.com>
2020-07-18 20:30:27 +0800

    修改了 toast 组件名

--- src/toast.vue
+++ src/toast.vue
@@ -15,7 +15,7 @@
 <script>
     //构造组件的选项
     export default {
-        name: 'GuluToast',
+        name: 'YixinToast',
         props: {
             autoClose: {
                 type: [Boolean, Number],
